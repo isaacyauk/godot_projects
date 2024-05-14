88b11b860ebe1a525d4595b729531992a01e8018
Isaac Yauk <isaacyauk@yahoo.com>
2024-05-14 12:10:41 -0600

    modifies interact prompt and location

--- Untitled_1st-Person-Shooter/scripts/simple-button.gd
+++ Untitled_1st-Person-Shooter/scripts/simple-button.gd
@@ -12,7 +12,7 @@ func get_prompt():
 	for action in InputMap.action_get_events(prompt_action):
 		if action is InputEventKey:
 			key_name = OS.get_keycode_string(action.physical_keycode)
-	return prompt_message + "\n[" + key_name + "]"
+	return prompt_message + " [" + key_name + "]\n\n\n"
 	
 
 func interact(body):
