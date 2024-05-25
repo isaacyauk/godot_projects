a866542e5baeba0c49ea43e9e60dbdda17166abe
Isaac Yauk <isaacyauk@yahoo.com>
2024-05-25 09:39:12 -0600

    adds a more updated player comntroller

--- Untitled_1st-Person-Shooter/scripts/player-UPDATE.gd
+++ Untitled_1st-Person-Shooter/scripts/player-UPDATE.gd
@@ -7,11 +7,14 @@ var _mouse_input : bool = false
 var _mouse_rotation : Vector3
 var _rotation_input : float
 var _tilt_input : float
+var _player_rotation : Vector3
+var _camera_rotation : Vector3
 
 # These titl vars allow you to clamp the camera looking to straight up and down. @export for quick tweaking.
 @export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
 @export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
-@export var CAMERA_CONTROLLER : Camera3D
+@export var CAMERA_CONTROLLER : Node3D
+@export var MOUSE_SENSITIVITY : float = 0.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -24,8 +27,8 @@ func _input(event):
 func _unhandled_input(event):
 	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
 	if _mouse_input:
-		_rotation_input = -event.relative.x
-		_tilt_input = -event.relative.y
+		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
+		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
 
 
 func _update_camera(delta):
@@ -35,18 +38,25 @@ func _update_camera(delta):
 	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
 	_mouse_rotation.y += _rotation_input * delta
 	
-	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_mouse_rotation)
+	_player_rotation = Vector3(0.0, _mouse_rotation.y, 0.0)
+	_camera_rotation = Vector3(_mouse_rotation.x, 0.0, 0.0)
+	
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
 	CAMERA_CONTROLLER.rotation.z = 0.0 # Avoid twisting camera
+	
+	global_transform.basis = Basis.from_euler(_player_rotation)
 	
 	_rotation_input = 0.0
 	_tilt_input = 0.0
 
 
 func _ready():
+	Global.player = self # When the player controller loads, this refreneces itself to the global var	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta):
+	Global.debug.add_property("MovementSpeed", SPEED, 1) # This is adding this value to the debug pannel using the custom methods in the debug class
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 		
